c6f3e3b0188b4abe1581136196aa3b5cf8f89bd5
tongvantruong <tong.tvt.truong@gmail.com>
2024-11-22 18:48:34 +0700

    add vitest

---
+++ composables/useSkill.test.ts
@@ -0,0 +1,20 @@
+import { it, expect, expectTypeOf } from "vitest";
+import { useSkill } from "./useSkill";
+
+it("should have 16 skills", () => {
+  const { skills } = useSkill();
+  expect(skills.length).toBe(16);
+});
+
+it("should have name and icon attributes", () => {
+  const { skills } = useSkill();
+  expectTypeOf(skills[0]).toEqualTypeOf<{ name: string; icon: string }>;
+});
+
+it("should shuttle the items every time is used", () => {
+  const { skills } = useSkill();
+  const firstTime = skills[0].name;
+
+  const { skills: secondSkills } = useSkill();
+  expect(secondSkills[0].name).not.toBe(firstTime);
+});
